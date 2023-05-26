3eac6deead98bc189efc7790918fde717abe3b47
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-05-26 23:55:50 +0200

    Login y Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import './App.css'
-import ResponsiveDrawer from './components/ResponsiveDrawer/ResponsiveDrawer';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import { Container } from '@mui/material';
+import ResponsiveDrawer from './components/ResponsiveDrawer/ResponsiveDrawer';
+import SignUp from './components/Authentication/SignUp/SignUp';
+import SignIn from './components/Authentication/SignIn/SignIn';
 
 const App = () => {
-
-return (
+ 
+  return (
     <Container>
       <div className="App">
+        {/* <SignUp/> */}
+        {/* <SignIn/> */}
         <ResponsiveDrawer/>
       </div>
     </Container>
-    );
-  }  
+  );
+};
 
-export default App
+export default App;
