4e5835f0202358ab3de5e98b2b830d4fbd2c5af1
Antonio Zurano Blazquez <azurano@outlook.es>
2023-06-22 01:14:05 +0200

    uncomment GetMyProfile

--- src/components/Authentication/GetMyProfile.jsx
+++ src/components/Authentication/GetMyProfile.jsx
@@ -1,23 +1,23 @@
-//   const [profile, setProfile] = useState({})
-//   const [loading, setLoading] = useState(false)
+   const [profile, setProfile] = useState({})
+   const [loading, setLoading] = useState(false)
 
-//   useEffect(() => {
-//     getMyProfile() 
-//   }, [])
+   useEffect(() => {
+     getMyProfile() 
+   }, [])
 
-//   const getMyProfile = async () => {
-//   setLoading(true);
-//     try {
-//       const response = await axios.get(`http://localhost:${process.env.PORT || 8000}/me`, {headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}})
-//       console.log("response", response)
-//       setProfile(response.data)
-//     } catch (error) {
-//       console.log("error", error)
-//       setError(error.response.data.error.result);
-//       setTimeout(() => {
-//         setError("");
-//       },5000)
-//     }
-//     setLoading(false);
-//   }
+   const getMyProfile = async () => {
+   setLoading(true);
+     try {
+       const response = await axios.get(`http://localhost:${process.env.PORT || 8000}/me`, {headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}})
+       console.log("response", response)
+       setProfile(response.data)
+     } catch (error) {
+       console.log("error", error)
+       setError(error.response.data.error.result);
+       setTimeout(() => {
+         setError("");
+       },5000)
+     }
+     setLoading(false);
+   }
 
