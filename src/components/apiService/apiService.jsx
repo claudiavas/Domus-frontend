1594bbe85167d15128d7d1c6fd93ced6e435c6ea
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-07-05 12:30:40 +0200

    fixing duplicated function

--- src/components/apiService/apiService.jsx
+++ src/components/apiService/apiService.jsx
@@ -86,8 +86,3 @@ export const sendPasswordResetEmail = async (body) => {
   console.log(data);
   return data;
 }
-
-export const updateUser = async (_id, body) => {
-  const { data } = await axios.put(`http://localhost:${PORT}/user/${_id}`, body);
-  return data;
-}
