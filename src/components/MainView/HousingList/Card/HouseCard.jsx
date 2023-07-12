d20941d0943ee370a20c121a3d007465a6ee4d8d
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2023-07-12 17:35:31 +0200

    User info completed and new iconsfor housing

--- src/components/MainView/HousingList/Card/HouseCard.jsx
+++ src/components/MainView/HousingList/Card/HouseCard.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button, CardActionArea, CardActions, Box, Card, CardContent, CardMedia, Divider, Avatar, Chip } from '@mui/material';
+import { Button, CardActionArea, CardActions, Box, Card, CardContent, CardMedia, Divider, Avatar, Chip, AlertTitle, Tooltip, IconButton } from '@mui/material';
 import { PhotoCarousel } from './PhotoCarousel';
 import BedOutlinedIcon from '@mui/icons-material/BedOutlined';
 import BathtubIcon from '@mui/icons-material/Bathtub';
@@ -10,166 +10,181 @@ import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from '../../../Contexts/AuthContext';
-import { TranslationContext } from '../../../Contexts/TranslationContext';
+import CardMembershipIcon from '@mui/icons-material/CardMembership';
+import { PhoneNumber } from './PhoneNumber';
+import { WhatsAppButton } from './WhattsappButton';
 
+export default function HouseCard({ _id, user, showRealEstateLogo, province, municipality, population, neighborhood, images, currency, price, squareMeters, rooms, transaction, type, furnished, garages, baths, title }) {
+  const { profile } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const showThumbsValue = false;
+  const precioxm2 = (price / squareMeters).toFixed(0);
+  let currencySymbol = '';
+  if (currency === 'USD') {
+    currencySymbol = '$';
+  } else if (currency === 'EUR') {
+    currencySymbol = '€';
+  }
 
-export default function HouseCard ({_id, province, municipality, population, neighborhood, 
-  description, currency, price, squareMeters, rooms, transaction, type, furnished, garages, baths}) {
-    
-    const navigate = useNavigate()
-    const showThumbsValue = false;
-
-    const { profile } = useContext(AuthContext);
-    const translations = useContext(TranslationContext);
-
-    // console.log("translations.type:", translations.type);
-
-    const precioxm2 = (price / squareMeters).toFixed(0);
-
-    let currencySymbol = '';
-    if (currency === 'USD') {
-      currencySymbol = '$';
-    } else if (currency === 'EUR') {
-      currencySymbol = '€';
-    }
-
-    // LE DAMOS FORMATO AL PRECIO
-
-    const formattedPrice = (new Intl.NumberFormat('es-ES', { minimumFractionDigits: 0, maximumFractionDigits: 0 }))
+  const formattedPrice = (new Intl.NumberFormat('es-ES', { minimumFractionDigits: 0, maximumFractionDigits: 0 }))
     .format(price)
     .replace('.', ' ')
     .replace(',', ',');
 
-    // LE DAMOS FORMATO A LA UBICACIÓN
-    
-    const removeTextInParentheses = (text) => {
-      return text.replace(/\([^()]*\)/g, "").trim()
-    };
-    
-    const locationText = [
-      province.PRO,
-      municipality.DMUN50,
-      population.NENTSI50,
-      neighborhood.NNUCLE50
-    ]
-      .filter(Boolean)
-      .filter((value, index, self) => self.indexOf(value) === index)
-      .map(removeTextInParentheses)
-      .join(", ")
-    
+  const removeTextInParentheses = (text) => {
+    return text.replace(/\([^()]*\)/g, "").trim()
+  };
 
-      return (
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 3,
-            display: 'flex',
-            padding: 0,
-            margin: '-5px 15px 20px -20px',
-            height: '230px'
-          }}
-        >
-          <span style={{ flex: '1 0 39%' }}>
-            {/* LEFT SIDE */}
-            <Card style={{ height: 230 }}>
-              <PhotoCarousel showThumbs={showThumbsValue} style={{ height: '100%'}} />
-            </Card>
-          </span>
-      
-          <span style={{ flex: '1 0 45%', marginLeft: '10px', marginRight: '10px', padding: 0  }}>
-            {/* CENTER */}
-            <span style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-              {/* TOP, CENTER */}
-              <Card style={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column', margin: '0px 0px 8px 0px'}}>
-              <div style={{ display: 'inline-flex', margin: '10px 10px 8px 5px' }}>
-                {/* <Chip label={translations['transaction']} color="primary" variant="contained" size="small" style={{ marginRight: '15px'}}/>
-                <Chip label={translations[lang].type[{type}]} color="primary" variant="outlined" size="small" style={{ marginRight: '15px'}} />
-                <Chip label={translations['furnished']} color="primary" variant="outlined" size="small" style={{ marginRight: '15px'}}/> */}
-              </div>
-                <h4 style={{ margin: '5px 5px 5px 5px', marginBottom: '5px', flexGrow: 1 }}>{description}</h4>
-                <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px', flexGrow: 1 }}>
-                  <LocationOnOutlinedIcon style={{ marginRight: '5px' }} />
-                  <h6 style={{ margin: '0px' }}>{locationText}</h6>
+  const locationText = [
+    province.PRO,
+    municipality.DMUN50,
+    population.NENTSI50,
+    neighborhood.NNUCLE50
+  ]
+    .filter(Boolean)
+    .filter((value, index, self) => self.indexOf(value) === index)
+    .map(removeTextInParentheses)
+    .join(", ");
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 3,
+        display: 'flex',
+        padding: 0,
+        margin: '-5px 15px 20px -20px',
+        height: '230px'
+      }}
+    >
+      <span style={{ flex: '1 0 39%' }}>
+        {/* LEFT SIDE */}
+        <Card style={{ height: 230 }}>
+          <PhotoCarousel showThumbs={showThumbsValue} images={images} style={{ height: '100%' }} />
+        </Card>
+      </span>
+
+      <span style={{ flex: '1 0 45%', marginLeft: '10px', marginRight: '10px', padding: 0 }}>
+        {/* CENTER */}
+        <span style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          {/* TOP, CENTER */}
+          <Card style={{ flex: '1 0 auto', display: 'flex', flexDirection: 'column', margin: '0px 0px 8px 0px' }}>
+            <div style={{ display: 'inline-flex', margin: '10px 10px 8px 5px' }}>
+              {
+                <>
+                  <Chip label={transaction} color="primary" variant="contained" size="small" style={{ marginRight: '15px' }} />
+                  <Chip label={type} color="primary" variant="outlined" size="small" style={{ marginRight: '15px' }} />
+                  {furnished && <Chip label={furnished} color="primary" variant="outlined" size="small" style={{ marginRight: '15px' }} />}
+                </>
+              }
+            </div>
+            <h4 style={{ margin: '5px 5px 5px 5px', marginBottom: '5px', flexGrow: 1 }}>{title}</h4>
+            <div style={{ display: 'flex', alignItems: 'center', marginBottom: '5px', flexGrow: 1 }}>
+              <LocationOnOutlinedIcon style={{ marginRight: '5px' }} />
+              <h6 style={{ margin: '0px' }}>{locationText}</h6>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', margin: '0px', padding: 0, marginBottom: '5px', flexGrow: 1 }}>
+              <FullscreenOutlinedIcon />
+              <h5 style={{ margin: '0px', marginLeft: '5px', marginRight: '50px' }}>{squareMeters} m2</h5>
+              <BedOutlinedIcon style={{ marginRight: '10px' }} />
+              <h5 style={{ margin: '0px' }}>{rooms}</h5>
+              {baths ? (
+                <div style={{ display: 'flex', alignItems: 'center', margin: '0px', padding: 0, marginBottom: '5px', marginLeft: "60px", flexGrow: 1 }}>
+                  <BathtubIcon style={{ marginRight: '10px' }} />
+                  <h5 style={{ margin: '0px' }}>{baths}</h5>
                 </div>
+              ) : (
+                <div></div>
+              )}
+              {garages ? (
                 <div style={{ display: 'flex', alignItems: 'center', margin: '0px', padding: 0, marginBottom: '5px', flexGrow: 1 }}>
-                  <FullscreenOutlinedIcon />
-                  <h5 style={{ margin: '0px', marginLeft: '5px', marginRight: '50px' }}>{squareMeters} m2</h5>
-                  <BedOutlinedIcon style={{ marginRight: '10px' }} />
-                  <h5 style={{ margin: '0px' }}>{rooms}</h5>
-                  {baths ? 
-                    <div style={{ display: 'flex', alignItems: 'center', margin: '0px', padding: 0, marginBottom: '5px', marginLeft: "60px", flexGrow: 1 }}>
-                      <BathtubIcon style={{ marginRight: '10px' }} />
-                      <h5 style={{ margin: '0px' }}>{baths}</h5>
-                    </div> :
-                    <div></div>
-                  }
-                  {garages ?
-                    <div style={{ display: 'flex', alignItems: 'center', margin: '0px', padding: 0, marginBottom: '5px', flexGrow: 1 }}>
-                      <DirectionsCarIcon style={{ marginRight: '10px' }} />
-                      <h5 style={{ margin: '0px' }}>{garages}</h5>
-                    </div> :
-                    <div></div>
-                  }
+                  <DirectionsCarIcon style={{ marginRight: '10px' }} />
+                  <h5 style={{ margin: '0px' }}>{garages}</h5>
                 </div>
-              </Card>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </Card>
 
-      
-              {/* BOTTOM, CENTER */}
-              <Card style={{ flex: '0 0 auto', display: 'flex', flexDirection: 'column' }}>
-                <div style={{ marginTop: '0px', marginLeft: '5px', padding: "4px" }}>
-                  <h4 style={{ margin: '0px', padding: 0, color: "#1976d2", display: "flex", justifyContent: 'space-between', alignItems: "center" }}>
-                    {formattedPrice} {currencySymbol}  <div>precio/m2: {precioxm2} {currencySymbol} </div>
-                    <Button onClick={() => navigate(`/housingdetails/${_id}`)} color="primary" variant="outlined">Ver Más</Button>
-                  </h4>
-                </div>
-              </Card>
-            </span>
-          </span>
-      
-          <span>
-            {/* RIGHT */}
-            <Card style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%' }}>
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
-                {profile.realEstateLogo && profile.avatar ? (
-                  <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                    <div>{profile.avatar}</div>
-                    <div>{profile.realEstateLogo}</div>
-                  </div>
+
+          {/* BOTTOM, CENTER */}
+          <Card style={{ flex: '0 0 auto', display: 'flex', flexDirection: 'column' }}>
+            <div style={{ marginTop: '0px', marginLeft: '5px', padding: "4px" }}>
+              <h4 style={{ margin: '0px', padding: 0, color: "#1976d2", display: "flex", justifyContent: 'space-between', alignItems: "center" }}>
+                {formattedPrice} {currencySymbol}  <div>precio/m2: {precioxm2} {currencySymbol} </div>
+                <Button onClick={() => navigate(`/housingdetails/${_id}`)} color="primary" variant="outlined">Ver Más</Button>
+              </h4>
+            </div>
+          </Card>
+        </span>
+      </span>
+
+      <span>
+        {/* RIGHT */}
+        <Card style={{ padding: "15px 3px 5px 3px", display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', width: "150px"}}>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
+            {user.realEstateLogo && showRealEstateLogo && user.profilePicture ? (
+              <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                <div><Avatar alt="profile picture" src={user.profilePicture} sx={{ width: 56, height: 56 }} /></div>
+                <div><Avatar alt="real estate logo" src={user.realEstateLogo} sx={{ width: 75, height: 75 }} /></div>
+              </div>
+            ) : (
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {user.profilePicture ? (
+                  <div><Avatar alt="profile picture" src={user.profilePicture} sx={{ width: 56, height: 56 }} /></div>
                 ) : (
-                  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    {profile.avatar ? (
-                      profile.avatar
-                    ) : (
-                      <Avatar sx={{ width: 55, height: 55 }}/>
-                    )}
-                  </div>
+
+                  <Avatar sx={{ width: 56, height: 56 }} />
                 )}
               </div>
-              <div style={{ alignSelf: 'center', marginTop: '10px' }}>
-                <h4 style={{ fontWeight: 'bold', textAlign: 'center', margin: '0px' }}>
-                  {profile.name} {profile.surname}
-                </h4>
-              </div>
-              <div style={{ marginTop: 'auto' }}>
-                {profile.phone && (
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <LocalPhoneOutlinedIcon fontSize='extrasmall' style={{ marginRight: '5px' }}/>
-                    <span><h5>{profile.phone}</h5></span>
-                  </div>
-                )}
-                {profile.email && (
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <EmailOutlinedIcon fontSize='extrasmall' style={{ marginRight: '5px' }} />
-                    <span><h6>{profile.email}</h6></span>
-                  </div>
-                )}
-              </div>
-            </Card>
-          </span>
+            )}
+          </div>
+
+          <div style={{ alignSelf: 'center', marginTop: '10px' }}>
+            <h4 style={{ fontWeight: 'bold', textAlign: 'center', margin: '0px' }}>
+              {user.name} {user.surname}
+            </h4>
+          </div>
+
+          {user.agentRegistrationNumber && user.agentRegistrationCommunity &&
+            <Tooltip title={`Registro No. ${profile.agentRegistrationNumber} C.A. de ${profile.agentRegistrationCommunity}`}>
+              <IconButton
+                size="small"
+                style={{ marginBottom: '5px' }}
+                color="success"
+              >
+                <CardMembershipIcon fontSize="medium" />
+              </IconButton>
+            </Tooltip>}
 
 
 
-        </Box>
-      )
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
+
+          {user.telephone1 && 
+            <WhatsAppButton phoneNumber={user.telephone1} />}
+           
+
+          {user.telephone2 && 
+            <PhoneNumber phoneNumber={user.telephone2} />}
+
+          <Tooltip title={user.email} arrow>
+            <IconButton
+              component="a"
+              href={`mailto:${user.email}`}
+              size="small"
+              style={{ marginBottom: '5px' }}
+              color="primary"
+            >
+              <EmailOutlinedIcon fontSize="medium" />
+            </IconButton>
+          </Tooltip>
+
+          </div>
+
+        </Card>
+      </span>
+    </Box >
+  )
 }
-      
